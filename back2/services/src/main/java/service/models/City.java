ce0c82409149ead82fadd1d186564818685b71b9
drsarya <drsarya043@gmail.com>
2021-05-04 13:23:15 +0300

    init commit

---
+++ back2/services/src/main/java/service/models/City.java
@@ -0,0 +1,24 @@
+package service.models;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.immutables.value.Value;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+    String PlaceId;
+
+    String PlaceName;
+
+    String CountryId;
+
+    String RegionId;
+
+    String CityId;
+
+    String CountryName;
+}
---
+++ back2/src/main/java/com/example/demo/api/FlightsApi.java
@@ -0,0 +1,8 @@
+package com.example.demo.api;
+
+public class FlightsApi {
+    //getAllFlights
+    //recent flights
+    //add recent
+    //searchFlight
+}
